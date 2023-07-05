310b575b6a03bc65fc554807f63142927a5259ea
silva4dev <web4dev.lucas@outlook.com>
2023-07-05 20:06:56 -0300

    chore: change routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,7 +3,6 @@ import { Router } from 'express'
 const routes = Router()
 
 routes.get('/', (request, response) => {
-  throw new Error('Acesso negado')
   return response.json({ message: 'Olá Dev!' })
 })
 
