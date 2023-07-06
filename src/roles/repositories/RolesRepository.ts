ae9fca2c0bba534175d443b1aef53aab1efee14e
silva4dev <web4dev.lucas@outlook.com>
2023-07-05 21:49:41 -0300

    chore: change roles repository

--- src/roles/repositories/RolesRepository.ts
+++ src/roles/repositories/RolesRepository.ts
@@ -11,7 +11,7 @@ export class RolesRepository {
     this.roles = []
   }
 
-  create({ name }: CreateRoleDTO) {
+  create({ name }: CreateRoleDTO): Role {
     const role = new Role()
 
     Object.assign(role, {
@@ -21,5 +21,9 @@ export class RolesRepository {
 
     this.roles.push(role)
     return role
+  }
+
+  findAll(): Role[] {
+    return this.roles
   }
 }
