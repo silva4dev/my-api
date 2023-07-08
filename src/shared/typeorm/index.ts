f081e176cfe810820da9036d347f4d20ae954b5e
silva4dev <web4dev.lucas@outlook.com>
2023-07-08 01:20:42 -0300

    chore: change typeorm

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,8 +1,9 @@
 import { DataSource } from 'typeorm'
+import { CreateRolesTable1688786922324 } from './migrations/1688786922324-CreateRolesTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [],
-  migrations: [],
+  migrations: [CreateRolesTable1688786922324],
 })
