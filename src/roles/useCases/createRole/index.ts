965e6681d3db38f323004604ba089c1e9fcbb51e
silva4dev <web4dev.lucas@outlook.com>
2023-07-19 20:06:01 -0300

    chore: remove index

--- src/roles/useCases/createRole/index.ts
+++
@@ -1,9 +0,0 @@
-import { RolesRepository } from '@roles/repositories/RolesRepository'
-import { CreateRoleUseCase } from './CreateRoleUseCase'
-import { CreateRoleController } from './CreateRoleController'
-
-const rolesRepository = RolesRepository.getInstance()
-const createRoleUseCase = new CreateRoleUseCase(rolesRepository)
-const createRolesController = new CreateRoleController(createRoleUseCase)
-
-export { createRolesController }
