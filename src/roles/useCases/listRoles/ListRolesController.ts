0d034f1bc00a558f3d4fd50afd931cee449eb9e3
silva4dev <web4dev.lucas@outlook.com>
2023-07-19 23:37:08 -0300

    chore: change list roles controller

--- src/roles/useCases/listRoles/ListRolesController.ts
+++ src/roles/useCases/listRoles/ListRolesController.ts
@@ -1,16 +1,14 @@
 import { Request, Response } from 'express'
-import { ListRolesUseCase } from './ListRolesUseCase'
 import { container } from 'tsyringe'
+import { ListRolesUseCase } from './ListRolesUseCase'
 
 export class ListRolesController {
   async handle(request: Request, response: Response): Promise<Response> {
     const listRolesUseCase = container.resolve(ListRolesUseCase)
-
     const page =
       request.query.page && Number(request.query.page) > 0
         ? Number(request.query.page)
         : 1
-
     const limit =
       request.query.limit && Number(request.query.limit) > 0
         ? Number(request.query.limit)
