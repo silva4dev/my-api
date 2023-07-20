4e054164c7ee935e8d1c7a07fdf2e3bc15bbff9f
silva4dev <web4dev.lucas@outlook.com>
2023-07-19 23:36:42 -0300

    feat: add container roles

---
+++ src/roles/container/index.ts
@@ -0,0 +1,20 @@
+import { container } from 'tsyringe'
+import { IRolesRepository } from '@roles/repositories/IRolesRepository'
+import { RolesRepository } from '@roles/repositories/RolesRepository'
+
+import { CreateRoleController } from '@roles/useCases/createRole/CreateRoleController'
+import { DeleteRoleController } from '@roles/useCases/deleteRole/DeleteRoleController'
+import { ListRolesController } from '@roles/useCases/listRoles/ListRolesController'
+import { ShowRoleController } from '@roles/useCases/showRole/ShowRoleController'
+import { UpdateRoleController } from '@roles/useCases/updateRole/UpdateRoleController'
+
+container.registerSingleton<IRolesRepository>(
+  'RolesRepository',
+  RolesRepository,
+)
+
+container.registerSingleton('CreateRoleController', CreateRoleController)
+container.registerSingleton('ListRolesController', ListRolesController)
+container.registerSingleton('ShowRolesController', ShowRoleController)
+container.registerSingleton('UpdateRoleController', UpdateRoleController)
+container.registerSingleton('DeleteRoleController', DeleteRoleController)
