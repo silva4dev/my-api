283da10ed193ce476dc94e8843050a8ef5adba1b
silva4dev <web4dev.lucas@outlook.com>
2023-07-19 21:04:58 -0300

    chore: change delete role use case

--- src/roles/useCases/deleteRole/DeleteRoleUseCase.ts
+++ src/roles/useCases/deleteRole/DeleteRoleUseCase.ts
@@ -1,12 +1,17 @@
 import { RolesRepository } from '@roles/repositories/RolesRepository'
 import { AppError } from '@shared/errors/AppError'
+import { inject, injectable } from 'tsyringe'
 
 type DeleteRoleParams = {
   id: string
 }
 
+@injectable()
 export class DeleteRoleUseCase {
-  constructor(private rolesRepository: RolesRepository) {}
+  constructor(
+    @inject('RolesRepository')
+    private rolesRepository: RolesRepository,
+  ) {}
 
   async execute({ id }: DeleteRoleParams): Promise<void> {
     const role = await this.rolesRepository.findById(id)
