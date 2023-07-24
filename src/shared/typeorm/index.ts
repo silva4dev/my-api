ecb420dfc3ddc92a7454c49092a7396ba9e54a3f
silva4dev <web4dev.lucas@outlook.com>
2023-07-24 19:55:47 -0300

    chore: change typeorm

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -2,10 +2,15 @@ import { DataSource } from 'typeorm'
 import { CreateRolesTable1688786922324 } from './migrations/1688786922324-CreateRolesTable'
 import { Role } from '@roles/entities/Role'
 import { CreateUsersTable1690238192108 } from './migrations/1690238192108-CreateUsersTable'
+import { AddRoleIdToUsersTable1657997456404 } from './migrations/1690238725006-AddRoleIdToUsersTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1688786922324, CreateUsersTable1690238192108],
+  migrations: [
+    CreateRolesTable1688786922324,
+    CreateUsersTable1690238192108,
+    AddRoleIdToUsersTable1657997456404,
+  ],
 })
